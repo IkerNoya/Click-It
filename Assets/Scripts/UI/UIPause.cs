330f4d952d2392b8456b2e5ce2a6700639efffdb
Iker <ikernoya4@gmail.com>
2020-06-06 14:08:07 -0300

    Created Pause scene

---
+++ Assets/Scripts/UI/UIPause.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIPause : MonoBehaviour
+{
+    public void OnClickContinue()
+    {
+        SceneManager.UnloadSceneAsync("Pause");
+    }
+    public void OnClickSave()
+    {
+        throw new NotImplementedException();
+    }
+    public void OnClickMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+}
